d0f8747cb085df5728497fa627c6fc3ea7d58348
Anton Ruzanov <antonru924@gmail.com>
2023-03-17 22:17:05 +0300

    New html files. Urls updated

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:recipe_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('recipe/<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
