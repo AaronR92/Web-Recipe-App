d5ef1e052b0bb91c1a059e2434819543c9cd6c65
Anton <86726028+AaronR92@users.noreply.github.com>
2023-03-17 12:48:23 +0300

    models.py created

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Direction(models.Model):
+    direction = models.CharField(max_length=200)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    pub_date = models.DateTimeField('date published')
+    description = models.CharField(max_length=400)
+    directions = models.ForeignKey(Direction, models.CASCADE)
+    ingredients = models.ForeignKey(Ingredient, models.PROTECT)
