8d56042114c369672337f10b005a515e345ef825
Anton Ruzanov <antonru924@gmail.com>
2023-03-17 22:16:34 +0300

    New html files

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
+from django.utils import timezone
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Ingredient
 
-def index(request):
-    latest_recipe_list = Recipe.objects.order_by('-pub_date')[:5]
-    output = ', '.join([recipe.name for recipe in latest_recipe_list])
-    return HttpResponse(output)
+class IndexView(generic.ListView):
+    template_name = 'recipes/index.html'
+    context_object_name = 'latest_recipe_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published recipes (not including those set to be
+        published in the future).
+        """
+        return Recipe.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
+    
+
+class DetailView(generic.DetailView):
+    model = Recipe
+    template_name = 'recipes/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any recipes that aren't published yet. 
+        """
+        return Recipe.objects.filter(pub_date__lte = timezone.now())
+
 
 def detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
